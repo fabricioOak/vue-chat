9693d7aeb2ad356cc3695fc5b45830c7985e8543
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-08-24 16:49:08 -0300

    update the chat page

---
+++ src/views/ChatView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ChatPageVue from "@/components/ChatPage.vue";
+import { socket } from "@/main";
+</script>
+
+<template>
+  <div>
+    <ChatPageVue :socket="socket" />
+  </div>
+</template>
