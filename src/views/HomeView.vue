5453be625a84fbcdcf0b07f33a0b399ea0cf542b
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-08-24 16:48:56 -0300

    update the login page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,34 @@
-<script setup lang="ts"></script>
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { socket } from "@/main";
+
+const router = useRouter();
+const userName = ref("");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  localStorage.setItem("userName", userName.value);
+  //sends the username and socket ID to the Node.js server
+  socket.emit("newUser", { userName: userName.value, socketID: socket.id });
+  router.push("/chat");
+};
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <form class="home-container">
+      <h2 class="home-header">Sign in</h2>
+      <label for="username">Username</label>
+      <input
+        class="username-input"
+        type="text"
+        v-model="userName"
+        minlength="6"
+        name="username"
+        id="username"
+      />
+      <button class="home-cta" @click="handleSubmit">Submit</button>
+    </form>
+  </main>
 </template>
